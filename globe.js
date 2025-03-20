64e7d8d2109cd63a8bef1360c5fddfd168013a46
chazera <hazera.c@gmail.com>
2025-03-20 18:08:15 +0100

    update

--- globe.js
+++ globe.js
@@ -1,22 +1,30 @@
+
 // init variables
 
 const urlAPI = "https://api.wheretheiss.at/v1/satellites/25544";
 const markerSvg = "<img src='image/iss.svg'></img>";
-const gData = [{
-    lat: 0,
-    lng: 0,
+const date = document.querySelector("#Date");
+const altitude = document.querySelector("#Altitude");
+const latitude = document.querySelector("#Latitude");
+const longitude = document.querySelector("#Longitude");
+const speed = document.querySelector("#Speed");
+const visibility = document.querySelector("#Visibility");
+const issData = [{
+    date: new Date().toLocaleDateString("fr"),
+    altitude: "Chargement...",
+    lat: "Chargement...",
+    lng: "Chargement...",
     size: 60,
-    color: 'yellow'
+    velocity: "Chargement...",
+    visibility: "Chargement...",
+
 }];
-let latitude;
-let longitude;
-let altitude;
 
 const globe = new Globe(document.querySelector('#globe'))
-    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
-    .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-    .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
-    .htmlElementsData(gData)
+   .globeImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-blue-marble.jpg')
+    .bumpImageUrl('//https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-topology.png')
+    .backgroundImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png')
+    .htmlElementsData(issData)
     .htmlElement(d => {
         const el = document.createElement('div');
 
@@ -31,7 +39,6 @@ const globe = new Globe(document.querySelector('#globe'))
         return el;
     })
 
-
 // Add auto-rotation
 globe.controls().autoRotate = true;
 globe.controls().autoRotateSpeed = 0.3;
@@ -39,8 +46,8 @@ globe.controls().autoRotateSpeed = 0.3;
 // init functions
 
 function updateISSPosition() {
-    globe.htmlLat(latitude);
-    globe.htmlLng(longitude);
+    globe.htmlLat(issData.lat);
+    globe.htmlLng(issData.lng);
 }
 
 async function getISSPosition() {
@@ -48,8 +55,11 @@ async function getISSPosition() {
         const response = await fetch(urlAPI);
         const data = await response.json();
 
-        latitude = data.latitude;
-        longitude = data.longitude;
+        issData[0].lat = data.latitude.toFixed(3);
+        issData[0].lng = data.longitude.toFixed(3);
+        issData[0].altitude = data.altitude.toFixed(0);
+        issData[0].velocity = data.velocity.toFixed(0);
+        issData[0].visibility = data.visibility;
         updateISSPosition();
     }
     catch (error) {
@@ -58,9 +68,38 @@ async function getISSPosition() {
     }
 }
 
+function dayOrNight(visibility) {
+    if (visibility === "daylight") {
+        return "Jour";
+    }
+    return "Nuit";
+}
+
+function showDataUI() {
+    date.innerText = issData[0].date;
+    altitude.innerText = `${issData[0].altitude} km`;
+    latitude.innerText = issData[0].lat;
+    longitude.innerText = issData[0].lng;
+    speed.innerText = `${issData[0].velocity} km/h`;
+    visibility.innerText = dayOrNight(issData[0].visibility);
+}
+
+function initiate() {
+    getISSPosition();
+    date.innerText = issData[0].date;
+    altitude.innerText = issData[0].altitude;
+    latitude.innerText = issData[0].lat;
+    longitude.innerText = issData[0].lng;
+    speed.innerText = issData[0].velocity;
+    visibility.innerText = "Chargement...";
+}
 
 // execute code
-getISSPosition();
+
+initiate();
 let intervalId = setInterval(() => {
     getISSPosition();
+    showDataUI();
 }, 3000);
+
+
